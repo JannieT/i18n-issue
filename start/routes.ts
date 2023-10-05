7a0a2e2142fe0cf17204414e4f23f7abab24ba07
Jannie Theunissen <jannie@scoutredeem.co>
2023-10-05 09:13:53 +0200

    starting to track changes

---
+++ start/routes.ts
@@ -0,0 +1,27 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer''
+|
+*/
+
+import Route from "@ioc:Adonis/Core/Route";
+
+Route.get("/", async (ctx) => {
+  ctx.i18n.switchLocale("af");
+  console.log(ctx.i18n.formatMessage("all.works")); // Dit werk!
+  return ctx.view.render("welcome");
+});
